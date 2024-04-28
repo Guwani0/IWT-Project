e994aa17bb12e8b6e70ff9cdd28b5698dd2aa61f
sasindu2003 <166760357+sasindu2003@users.noreply.github.com>
2024-04-28 20:56:00 +0530

    gallery page
    making structure

---
+++ Gallery.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+        <meta charset="UTF-8">
+        <title> Gallery </title>
+        <link rel="stylesheet" href="styles/homestyles.css">
+    </head>
+    <body>
+        <img src="images/Logo.jpg" alt="Logo" height="100px" width="150px" class="center">
+        <div class="navbar">
+            <ul> 
+                <li> <a href="HomePage.html">  Home </a> </li>
+                <li> <a href="#"> IMA 2024 </a> </li>
+                <li> <a href="#"> Archives </a> </li>
+                <li> <a href="#"> Gallery </a> </li>
+                <li> <a href="#"> The IMA Story </a> </li>
+                <li> <a href="#"> Download The App </a> </li>
+                <li> <a href="#"> Contact Us </a> </li>
+            </ul>
+        
+    </div>
+    </body>
+</html>
