4a8cefe81233e1c029f8319d12c15da4ef625af9
Guwani0 <166727273+Guwani0@users.noreply.github.com>
2024-04-28 12:04:19 +0530

    header

---
+++ Header.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+            <ul> 
+                <li> <a href="HomePage.html">  Home </a> </li>
+                <li> <a href="#"> IMA 2024 </a> </li>
+                <li> <a href="#"> Archives </a> </li>
+                <li> <a href="#"> Gallery </a> </li>
+                <li> <a href="#"> The IMA Story </a> </li>
+                <li> <a href="#"> Download The App </a> </li>
+                <li> <a href="#"> Contact Us </a> </li>
+            </ul>
+    </div>
