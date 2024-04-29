bc6146d005b3a8f9952302dcc10fdf7477aa0ccf
Amath Perera <jinuka.markperera@gmail.com>
2024-04-29 21:24:26 +0530

    changes

---
+++ Login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login| IMA </title>
+</head>
+<body>
+    <h1> LOG IN </h1>
+    <div>     
+    </div>
+</body>
+</html>
