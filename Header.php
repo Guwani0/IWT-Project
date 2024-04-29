82bdff02138257b3ee1f171f36b32add7e3de5e0
Guwani0 <166727273+Guwani0@users.noreply.github.com>
2024-04-29 17:20:08 +0530

    changed

--- Header.php
+++ Header.php
@@ -1,11 +1,33 @@
+ <!-- header.php -->
 <div class="navbar">
-            <ul> 
-                <li> <a href="HomePage.html">  Home </a> </li>
-                <li> <a href="#"> IMA 2024 </a> </li>
-                <li> <a href="#"> Archives </a> </li>
-                <li> <a href="#"> Gallery </a> </li>
-                <li> <a href="#"> The IMA Story </a> </li>
-                <li> <a href="#"> Download The App </a> </li>
-                <li> <a href="#"> Contact Us </a> </li>
-            </ul>
+    <div class="container">
+    <div class="logo">
+        <a href="index.php">
+            <img src="images/logo.png" alt="Logo" width="80" height="80"> 
+        </a>
     </div>
+
+    <nav class="navbar">
+        <ul> 
+            <li> <a href="./index.php"< ?php if(basename($_SERVER[?>>  Home </a> </li>
+            <li> <a href="#" <?php if(basename($_SERVER['PHP_SELF']) == '#') echo 'class="active"';?›> IMA 2024 </a> </li>
+            <li> <a href="#"> Archives </a> </li>
+            <li> <a href="#"> Gallery </a> </li>
+            <li> <a href="#"> The IMA Story </a> </li>
+            <li> <a href="#"> Download The App </a> </li>
+            <li> <a href="#"> Contact Us </a> </li>
+        </ul>
+    </nav>
+   
+<header class="header">
+    <!-- Logo that links to the homepage -->
+    <div class="logo">
+        <a href="index.php">
+            <img src="logo.png" alt="Company Logo" width="80" height="80"> <!-- Logo image -->
+        </a>
+    </div>
+
+‹li>a href="./apps-php"<?php if (basename($_SERVER[ 'PHP_SELF']) == 'apps.php") echo 'class="active"*; ?>>Apps‹/a> </11>
+«lisa href="/movies-php"<?php if(basename($_SERVER['PHP_SELF']) == 'movies-php) echo 'class="active"*; ?››Movies‹/a> </li>
+‹li><a href="./books-php"<?php if(basename ($_SERVER['PHP_SELF']) == 'books-php") echo 'class"active"*; ?>>Books</a› ‹/1i>
+«li>a href="./blog-php"<7php if(basename($_SERVER['PHP_SELF']) == 'blog-php") echo 'class-"active"'; ?››Blog</a> ‹/1i>
