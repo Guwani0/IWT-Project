77c30a02e49926204edff34b970f7d64dca4ab9a
Amath Perera <jinuka.markperera@gmail.com>
2024-04-30 20:26:22 +0530

    css error

--- Login.php
+++ Login.php
@@ -3,22 +3,25 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="loginstyles.css">
+        <link rel="stylesheet" href="styles/loginstyles.css">
         <title>LOG IN | IMA</title>
     </head>
     <body>
-        <h1 style="font-size: xxx-large;">LOG IN</h1>
+         <?php include 'header.php'; ?>
+        <h1 class="header" style="font-size: xxx-large;">LOG IN</h1>
         <div class="login-box">
             <form action="login.php" method="post">
                 <label for="username">Username</label><br>
                 <input type="text" required><br>
                 <label for="password">Password</label><br>
                 <input type="password" required><br>
-                <a href="password_reset.js" style="padding: 10px;">Forgot password?</a>
+                <div>
+                    <a href="#" class="fp">Forgot password?</a> 
+                </div>
                 <br><br>
                 <input type="submit" value="LOG IN">
                 <p style="text-align: center;">Don't have an account?</p>
-                <button><a href="#" class="signup-design">SIGN UP</a></button>
+                <button class="btn"><a href="#">SIGN UP</a></button>
             </form>
         </div>
     </body>
