5451c1f567af47d1e088f78d39ff3e61ee8a513e
Amath Perera <jinuka.markperera@gmail.com>
2024-04-30 09:28:55 +0530

    changes

--- Login.php
+++ Login.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login| IMA </title>
-</head>
-<body>
-    <h1> LOG IN </h1>
-    <div>     
-    </div>
-</body>
-</html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="loginstyles.css">
+        <title>LOG IN | IMA</title>
+    </head>
+    <body>
+        <h1 style="font-size: xxx-large;">LOG IN</h1>
+        <div class="login-box">
+            <form action="login.php" method="post">
+                <label for="username">Username</label><br>
+                <input type="text" required><br>
+                <label for="password">Password</label><br>
+                <input type="password" required><br>
+                <a href="password_reset.js" style="padding: 10px;">Forgot password?</a>
+                <br><br>
+                <input type="submit" value="LOG IN">
+                <p style="text-align: center;">Don't have an account?</p>
+                <button><a href="#" class="signup-design">SIGN UP</a></button>
+            </form>
+        </div>
+    </body>
