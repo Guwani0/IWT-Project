8954c1b84bd3565c2dbd16af6e0ce5035a24021f
AchineSanjana <achinesanjana2004@gmail.com>
2024-05-02 04:45:38 +0530

    Sanjana

--- ima_story.php
+++ ima_story.php
@@ -14,10 +14,35 @@
     background-attachment: fixed;
     margin: 0;
     padding: 0;
-    overflow-y: auto;
-
-  
+    overflow-y: auto;  
 }
+.textbox{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    font-family:Future;
+    font-size: 15px;
+    background-color:black;
+    text-align: center;
+    box-shadow:  0px 0px 10px rgba(0, 0, 0, 0.5);
+    padding: 7px;
+    
+}
+.subtitle{
+    font-size:x-large;
+    font-family:Future;
+    margin:auto;
+    text-align: center;
+    color: yellow;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+}
+.awardpic{
+    display: block;
+    margin: 0 auto; 
+    border-radius: 8px;
+    box-shadow:  0px 0px 10px rgba(0, 0, 0, 0.5);
+}
+
 
 </style>
         <title>IMA Story</title>
@@ -25,7 +50,7 @@
 
 <body>
 <?php include 'Header.php'?>
-backgo
+
 <div class="float">
 <div class="header">
     <h1 class="headtext">IMA </h1>
@@ -33,11 +58,58 @@ backgo
     <h1 class="headtext">  STORY</h1>
 </div>
 <br><br>
-<div>
-<img class="awardpic" src="../IWT-Project/images/awardpic.jpeg" alt="award picture">
+<img class="awardpic" src="../IWT-Project/images/awardpic.jpeg" alt="award picture" width="300" height="200">
 <br><br>
-<p>Like all things, 
+<div class="textbox">
+<p> Like all things, IMA has a beginning too.....<br>
+IMA, also known as International Music Award, was established in 2002 by the record label, Interscope records.<br>
+The awards show was founded by Jimmy Lovine and Ted Field, as a love letter to the music industry.<br>
+At the time of the awards formation, interscope was starting its golden era with artists like<br> Eminem, Black Eyed Peas and 50 Cent under their name.
+</p>
 </div>
+<br><br><br><br><br><br><br>
+<div>
+<h2 class="subtitle">More about Interscope Records</h2>
+</div>
+<br><br><br><br>
+<div class="header">
+   <img class="awardpic" src="../IWT-Project/images/artists3.jpeg" alt="artist pictures" width="" height="180"> 
+   <img class="awardpic" src="../IWT-Project/images/interscope2.png" alt="artist pictures" width="" height="180">
+</div>
+<br><br><br><br><br>
+<div class="textbox">
+<p> Interscope Records is a record house that is home to many popular artists like<br> Billie Eilish, Lana Del Ray and BLACKPINK.<br>
+    It was founded in 1990 in Santa Monica, California, United States.<br> Through the years, many Hall of Fame artists have walked<br> through their doors.
+    Interscope's contribution to the music industry has gained recognision World wide,<br> making Interscope of one the leading music producer and 
+    arist management agencies<br> in the world. </p>
+</div>
+<br><br><br><br><br><br><br><br>
+<div>
+<h2 class="subtitle">Values of IMA</h2>
+</div>
+<br><br>
+<div class="textbox">
+<p> <b>CREATIVITY</b>drives everything we do.<br> It pushes us to think outside of the box.<br> To innovate.<br>
+     To challenge the status quo.<br> To keep us from falling into the “that’s how we’ve always done it” 
+     mentality.<br> To ensure that we’re constantly evolving and improving – with a determined bias for action.<br>
+      We are confident enough to take on any endeavor, <br>but also humble enough to know that there is always 
+      room for<br> improvement.<br><br>
+
+
+<b>DIVERSITY</b> AND <b>INCLUSION</b><br> are fundamental because we know that like great music,<br> success requires bringing
+ together diverse voices. We strive to be as inclusive as the music and creators we represent. We prize the 
+ individual but we celebrate the team.<br><br>
+
+
+Everything we do, we do with <b>INTEGRITY</b>.<br> Our communication is candid and respectful.<br> Our work is always
+ guided by what is best for the music community.<br> It must be grounded in candor, while fostering a respectful
+  environment where<br> <b>COLLABORATION</b><br> thrives and great ideas take flight.<br><br>
+
+
+But if you really want to understand what drives us,<br> it’s <b>PASSION</b>.<br> We love music and every single person,
+ place or thing that touches it.<br> That’s why we do it.<br> And that’s the kind of group we want to be a part of. </p>
+</div>
+
 </div>
 
 </body>
