96d18adb1b2c1311be0c49450b7ca588fc185fa3
Amath Perera <jinuka.markperera@gmail.com>
2024-05-01 17:52:41 +0530

    changes and download pg

---
+++ download.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles/download.css">
+        <title>Mobile version download | IMA</title>
+    </head>
+    
+    <body>
+    <?php include 'header.php'; ?>
+        <div class="containerA">
+            <div class="left">
+                <div class="dpara1">
+                <span>IMA <br>Mobile App</span>
+                <div>
+                <img src="images/logo.png" alt="logo" class="rotate">
+                </div>
+                </div>
+        <div class="box">
+            <form action="download.php">
+                <p class="dpara2">Click on any to download</p><br>
+                <div class="pic">
+                <a href="https://appstoreconnect.apple.com/login"><img src="images/apple.png" alt="App store"></a>
+                <a href="https://play.google.com/store/account/rewards?pli=1"><img src="images/playstore.png" alt="Google store"></a>
+                <a href="https://apkpure.com/"><img src="images/apk.png" alt="download apk"></a>
+        </div>
+            </form>
+                </div>
+            </div>
+        <div class="right">
+        <div class="qr">
+            <img src="images/phone.png" alt="QR">
+        </div>
+        </div>
+        </div>
+    </body>
