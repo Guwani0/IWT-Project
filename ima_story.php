2da070eaafcecb2f67c6ad4834936ef0b6167460
AchineSanjana <achinesanjana2004@gmail.com>
2024-05-01 18:41:38 +0530

    Sanjana

--- ima_story.php
+++ ima_story.php
@@ -1,16 +1,42 @@
 <!DOCTYPE html>
-<head>
-    <title>IMA Story</title>
-    <link rel="stylesheet" href="ima_story.css">
-</head>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles/ima_story.css">
+        <style>
+        body {
+    background-image: url('../IWT-Project/images/back.png');
+    background-color:black;
+    background-size:cover; 
+    background-position:center;
+    background-repeat:no-repeat;
+    background-attachment: fixed;
+    margin: 0;
+    padding: 0;
+    overflow-y: auto;
+
+  
+}
+
+</style>
+        <title>IMA Story</title>
+    </head>
 
 <body>
 <?php include 'Header.php'?>
+backgo
 <div class="float">
 <div class="header">
-    <h1 class="headtext">IMA</h1>
+    <h1 class="headtext">IMA </h1>
     <img class="headlogo" src="../IWT-Project/images/mainLogo.png" alt="logo" width="100" heigh="100">
-    <h1 class="headtext">STORY</h1>
+    <h1 class="headtext">  STORY</h1>
+</div>
+<br><br>
+<div>
+<img class="awardpic" src="../IWT-Project/images/awardpic.jpeg" alt="award picture">
+<br><br>
+<p>Like all things, 
 </div>
 </div>
 
