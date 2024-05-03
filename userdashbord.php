8e2bdb9f11060b1c074eefab6c5a44b1ed012fcf
Amath Perera <jinuka.markperera@gmail.com>
2024-05-03 15:21:25 +0530

    ud

---
+++ userdashbord.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles/loginstyles.css">
+        <title>User Dashbord | IMA</title>
+        <style>body {
+    background-image: url('../IWT-Project/images/back.png');
+    background-color:black;
+    background-size:cover; 
+    background-position:center;
+    background-repeat:no-repeat;
+    background-attachment: fixed;
+    margin: 0;
+    padding: 0;
+    overflow-y: auto;
+}
+</style>
+    </head>
