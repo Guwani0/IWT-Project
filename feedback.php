7e627112158778930ff0fa49b8b37ed3dab625e6
sasindu2003 <166760357+sasindu2003@users.noreply.github.com>
2024-05-03 19:09:15 +0530

    feedback pge
    new pge

---
+++ feedback.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+        <meta charset="UTF-8">
+        <title> Gallery </title>
+        <link rel="stylesheet" href="styles/gall.css">
+        <link rel="stylesheet"
+        href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    </head>
+    <body>
+        
+      <div class="logo">
+        <img src="images/Two.png" alt="Logo" height="100px" width="100px"  >
+      </div>
+        <div class="navbar">
+            <ul> 
+                <li> <a href="HomePage.html">  Home </a> </li>
+                <li> <a href="#"> IMA 2024 </a> </li>
+                <li> <a href="#"> Archives </a> </li>
+                <li> <a href="#"> Gallery </a> </li>
+                <li> <a href="#"> The IMA Story </a> </li>
+                <li> <a href="#"> Download The App </a> </li>
+                <li> <a href="#"> Contact Us </a> </li>
+            </ul>
+    </div>
+    
+    
+
+
+
+    </body>
+</html>
