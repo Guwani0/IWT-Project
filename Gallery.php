4865d1f36698e600ee484dcb83be93199b3cf37e
sasindu2003 <166760357+sasindu2003@users.noreply.github.com>
2024-05-04 13:24:38 +0530

    addd header
    php file

--- Gallery.php
+++ Gallery.php
@@ -8,21 +8,12 @@
         href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     </head>
     <body>
-        
-      <div class="logo">
-        <img src="images/Two.png" alt="Logo" height="100px" width="100px"  >
-      </div>
-        <div class="navbar">
-            <ul> 
-                <li> <a href="HomePage.html">  Home </a> </li>
-                <li> <a href="#"> IMA 2024 </a> </li>
-                <li> <a href="#"> Archives </a> </li>
-                <li> <a href="#"> Gallery </a> </li>
-                <li> <a href="#"> The IMA Story </a> </li>
-                <li> <a href="#"> Download The App </a> </li>
-                <li> <a href="#"> Contact Us </a> </li>
-            </ul>
-    </div>
+      <?php
+
+      require 'header.php';
+
+      ?>
+
     <div class="head">
           <div class="t1">
             <header class="gallery">Gallery</header>
@@ -40,24 +31,24 @@
        <img src="images/R.jpeg" alt="">
        <h3>2023 highlights</h3>
        <div class="content">
-          <i class='bx bxs-camera'></i>
-          <a class="ll" href="highlights.html">20 more photos</a>
+          
+          <a class="ll" href="highlights.html">20  photos</a>
        </div>
       </div>
      <div class="pic">
        <img src="images/red.webp" alt="">
        <h3>Red Carpet</h3>
        <div class="content">
-          <i class='bx bxs-camera'></i>
-          <a href="redcarpet.html">20 photos</a>
+        
+          <a class="ll" href="redcarpet.html">20 photos</a>
        </div>
       </div>
      <div class="pic">
        <img src="images/perf.jpeg" alt="">
        <h3>IMA Performense</h3>
        <div class="content">
-         <i class='bx bxs-camera'></i>
-          <a href="performence.html">20 photos</a>
+         
+          <a class="ll" href="performence.html">20 photos</a>
        </div>
      </div>
   </div>
@@ -74,8 +65,8 @@
          <video src="video/4.mp4" controls autoplay mutes class="vv2"></video>
          <h3>Performances Highlight </h3>
           <div class="content">
-            <i class='bx bxs-video'></i>
-             <a href="performencehlight.html">5 Videos</a>
+            
+             <a class="ll" href="performencehlight.html">5 Videos</a>
            </div>
         </div>
     
@@ -84,8 +75,8 @@
             <video src="video/6.mp4" controls autoplay muted class="vv2"></video>
              <h3>Winners 2023</h3>
           <div class="content">
-            <i class='bx bxs-video'></i>
-              <a href="winners.html">5 Videos</a>
+            
+              <a class="ll" href="winners.html">5 Videos</a>
            </div>
         </div>
 
@@ -93,8 +84,8 @@
           <video src="video/4.mp4" controls autoplay muted class="vv2"></video>
          <h3>Host Rewind</h3>
           <div class="content">
-            <i class='bx bxs-video'></i>
-               <a href="hostrewind.html">5 Videos</a>
+      
+               <a class="ll" href="hostrewind.html">5 Videos</a>
            </div>
       </div>
    </div>   
