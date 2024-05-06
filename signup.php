52fe4c3bf30975c082e68ddf71cf4d21e7e1f3bf
Guwani0 <166727273+Guwani0@users.noreply.github.com>
2024-05-06 17:00:19 +0530

    guwani

--- signup.php
+++ signup.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles/header.css">
     <link rel="stylesheet" href="styles/register.css">
     <link rel="stylesheet" href="styles/navigation.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -49,6 +48,30 @@
             <p style="text-align: center;" class="para1"> Already have an account? <br> <br>
             <button type="submit" class="admin"> <a href="login.php" style="text-decoration:none; color:white"> LOG IN </button>
     </form>
+    
+    <footer>
+        <div class="footerContainer">
+            <div class="socialIcons">
+                <a href="https://www.facebook.com/"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://www.twitter.com/"><i class="fa-brands fa-twitter"></i></a>
+                <a href="https://www.google.com/"><i class="fa-brands fa-google-plus"></i></a>
+                <a href="https://www.youtube.com/"><i class="fa-brands fa-youtube"></i></a>
+            </div>
+            <div class="footerNav">
+                <ul><li><a href="index.php">Home</a></li>
+                    <li><a href="">Download The App</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Contact Us</a></li>
+                    <li><a href=""> FAQ </a></li>
+                </ul>
+            </div>
+            
+        </div>
+        <div class="footerBottom">
+            <p>&copy; 2024 IMA Media Networks, a division of IMA Global Inc. All Rights Reserved.</p>
+        </div>
+    </footer>
 
     </body>
 </html>
